29ca5bb69640c2bd31f64c365717e5631d9b07b4
mcc1200 <mcasas@utexas.edu>
2026-09-08 18:13:58 -0500

    addded slurm script for cic pdf computation

---
+++ bin/sense/submit_cic_pdf.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+
+'''
+Submit compute_cic_pdf.py as a regular (non-interactive) SLURM job, rather
+than waiting on the idev queue -- runs on a single node, using all its cores
+via the multiprocessing pool in compute_cic_pdf.py (nworkers picked up from
+SLURM_CPUS_ON_NODE).
+
+Idempotent -- safe to resubmit if a job doesn't finish in time (already
+processed files are skipped).
+'''
+
+def submit_cic_pdf(dataset='NLB', i0=0, i1=-1, overwrite=False,
+                    time=2, queue='normal'):
+    scriptdir = os.path.dirname(__file__)
+    hr = int(np.floor(time))
+    mn = int((time * 60) % 60)
+    os.makedirs('o', exist_ok=True)
+
+    a = '\n'.join([
+        '#!/bin/bash',
+        '#SBATCH -J cic.%s' % dataset,
+        '#SBATCH -o o/cic.%s.%%j.out' % dataset,
+        '#SBATCH -p %s' % queue,
+        '#SBATCH -N 1',
+        '#SBATCH -n 1',
+        '#SBATCH --time=%s:%s:00' % (str(hr).zfill(2), str(mn).zfill(2)),
+        '#SBATCH -A AST25023',
+        '',
+        'module purge',
+        'module load intel',
+        'module load impi',
+        'module load fftw3/3.3.10',
+        'module load gsl',
+        '',
+        'unset PYTHONPATH',
+        'source ~/.bashrc',
+        '',
+        'conda activate simbig',
+        '',
+        ''])
+
+    a += 'python %s/compute_cic_pdf.py %s %i %i%s\n' % (
+        scriptdir, dataset, i0, i1, ' --overwrite' if overwrite else '')
+
+    f = open(os.path.join(os.environ['WORK'], 'script.slurm'), 'w')
+    f.write(a)
+    f.close()
+    os.system('sbatch %s' % os.path.join(os.environ['WORK'], 'script.slurm'))
+    return None
+
+if __name__ == '__main__':
+    submit_cic_pdf(dataset='NLB', i0=0, i1=-1, time=2, queue='normal')
+    submit_cic_pdf(dataset='HOD', i0=0, i1=-1, time=1, queue='normal')
